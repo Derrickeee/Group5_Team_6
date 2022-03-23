c9dfd2f8b15db767c2e820b1ba02b207645b56c8
Derrickeee <2102080@sit.singaporetech.edu.sg>
2022-03-23 19:32:08 +0800

    commit

--- app.py
+++ app.py
@@ -13,12 +13,16 @@ console = Tk()
 
 def fileBrowser():
     global tf
+    global file_name
+    global file_path
     tf = filedialog.askopenfilename(
         initialdir="",
         title="Open Text file",
         filetypes=(("SMALI Files", "*.smali"),("APK Files", "*.apk"),("JAVA Files", "*.java"),("Kotlin Files","*.kt"))
     )
-
+    file_name = os.path.basename(tf)
+    file_path = tf
+    print(file_name, file_path)
     entry.insert(END, tf)
 
 
@@ -33,71 +37,67 @@ def find_files(filename, search_path):
    return result
 
 
-def proceed():  # will run if user selects the option to reupload dataset from the menu page
-    global tf
+def proceed():
+    # will run if user selects the option to reupload dataset from the menu page
+
     global filename
-    try:
-            tf = open(tf, 'r')
-            data = tf.read()
-            mainMenuFrame.pack_forget()
-            my_frame1.pack()
-            mainMenuFrame.pack_forget()
-            my_frame1.pack()
+    tfi = open(tf, 'r')
+    print(tfi)
+    data = tfi.read()
+    mainMenuFrame.pack_forget()
+    my_frame1.pack()
+    mainMenuFrame.pack_forget()
+    my_frame1.pack()
 
+    def mutliple_yview(*args):
+        txt1.yview(*args)
+        txt2.yview(*args)
 
+    def mutliple_xview(*args):
+        txt1.xview(*args)
+        txt2.xview(*args)
 
-            def mutliple_yview(*args):
-                txt1.yview(*args)
-                txt2.yview(*args)
-
-            def mutliple_xview(*args):
-                txt1.xview(*args)
-                txt2.xview(*args)
-
-            hor_scroll = Scrollbar(my_frame1, orient='horizontal')
-            hor_scroll.pack(side=BOTTOM, fill=X)
-            text_scroll = Scrollbar(my_frame1)
-            text_scroll.pack(side=RIGHT, fill=Y)
-            txt1 = Text(my_frame1, width=100, height=15, yscrollcommand=text_scroll.set
+    hor_scroll = Scrollbar(my_frame1, orient='horizontal')
+    hor_scroll.pack(side=BOTTOM, fill=X)
+    text_scroll = Scrollbar(my_frame1)
+    text_scroll.pack(side=RIGHT, fill=Y)
+    txt1 = Text(my_frame1, width=100, height=15, yscrollcommand=text_scroll.set
                 , wrap="none", xscrollcommand=hor_scroll.set)
-            txt1.pack(pady=0)
-            # SECOND textbox
-            txt2 = Text(my_frame1, width=100, height=15, yscrollcommand=text_scroll.set
+    txt1.pack(pady=0)
+    # SECOND textbox
+    txt2 = Text(my_frame1, width=100, height=15, yscrollcommand=text_scroll.set
                 , wrap="none", xscrollcommand=hor_scroll.set)
-            txt2.pack(pady=10)
-            text_scroll.config(command=mutliple_yview)
-            hor_scroll.config(command=mutliple_xview)
+    txt2.pack(pady=10)
+    text_scroll.config(command=mutliple_yview)
+    hor_scroll.config(command=mutliple_xview)
 
-            txt1.insert(END, data)
-            if tf[-4:].lower() in ['java']:
-                print("True")
-                javaFile = find_files(filename, './uploads/')[0]
-                print(javaFile)
-                rawFilename = javaFile.split('/')[-1]
-                # Obfuscate the java file uploaded
-                start = time.time()
-                javaobfuscator.main('C:/Users/alice/Documents/ICT1009/Lecture 3/Lecture3A.java')
+    txt1.insert(END, data)
+    if tf[-4:].lower() in ['java']:
+        print("True")
+        #javaFile = find_files(filename, './uploads/')[0]
+        #print(javaFile)
+        #rawFilename = javaFile.split('/')[-1]
+        # Obfuscate the java file uploaded
+        #start = time.time()
 
-                with open(rawFilename, 'r', encoding='utf-8-sig') as file:
-                    obfuscatedContents = file.read()
-                    txt2.insert(END, obfuscatedContents)
-                    file.close()
-            start = time.time()
-            originalSize = os.stat(data).st_size
-            end = time.time()
-            obfuscatedSize = os.stat(data).st_size
-            # Calculate size difference of the two MainActivity.smali in percentage
-            sizeDiff = (obfuscatedSize / originalSize) * 100
-            if sizeDiff > 0:
-                sizePercentage = '+' + str(round(sizeDiff, 2)) + '%'
+        #with open(rawFilename, 'r', encoding='utf-8-sig') as file:
+            #obfuscatedContents = file.read()
+            #txt2.insert(END, obfuscatedContents)
+            #file.close()
+    start = time.time()
+    originalSize = os.stat(data).st_size
+    end = time.time()
+    obfuscatedSize = os.stat(data).st_size
+    # Calculate size difference of the two MainActivity.smali in percentage
+    sizeDiff = (obfuscatedSize / originalSize) * 100
+    if sizeDiff > 0:
+        sizePercentage = '+' + str(round(sizeDiff, 2)) + '%'
 
-            else:
-                sizePercentage = '-' + str(round(sizeDiff, 2)) + '%'
+    else:
+        sizePercentage = '-' + str(round(sizeDiff, 2)) + '%'
 
+    tf.close()
 
-            tf.close()
-    except:
-        messagebox.showerror("File not selected!")
 
     """This function runs if user selects the option to reupload dataset from the menu page"""
   # prompts user to select file again
